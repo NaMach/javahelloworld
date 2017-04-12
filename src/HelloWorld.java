9a233eb7dab8255a5794518ed83aeee358a3abfd
NaMach <naren.wins@gmail.com>
2017-04-12 10:37:19 +0000

    Testing Docker Hub Auto Build

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World again");
+		System.out.println("Hello World");
+		System.out.println("Testing Docker Hub Auto Build CI");
+
 	}
 }
